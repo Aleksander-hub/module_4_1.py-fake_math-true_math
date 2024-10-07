cb912340100d071ee210cfab981522976ca0998e
Aleksander-hub <oshepkov77@gmail.com>
2024-10-07 17:14:41 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -1,9 +1,9 @@
-import math
+from math import inf
 
 def divide( first, second):
 
     if second >0:
         print(first/second)
     elif second == 0:
-        print(math.inf)
+        return print(inf)
 
